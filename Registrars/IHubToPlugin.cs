0ddc718d96d4c41ffedb36db92655851daebce3c
Jurnest <kyrdrms@gmail.com>
2026-03-28 19:27:46 +0300

    fix

--- Registrars/IHubToPlugin.cs
+++ Registrars/IHubToPlugin.cs
@@ -1,4 +1,5 @@
-﻿using HubTo.Abstraction.Logging;
+﻿using HubTo.Abstraction.Enums;
+using HubTo.Abstraction.Logging;
 using HubTo.Abstraction.Models;
 using System.Collections.Generic;
 using System.Threading;
@@ -9,8 +10,9 @@ namespace HubTo.Abstraction.Registrars
     public interface IHubToPlugin
     {
         string Name { get; }
+        PluginType Type { get; }
         Task InitializeAsync(IDictionary<string, string> settings, IHubToLogger logger, CancellationToken cancellationToken = default);
         Task ShutdownAsync(CancellationToken cancellationToken = default);
-        Task<HealthStatus> CheckHealthAsync(CancellationToken ct = default);
+        Task<HealthStatus> CheckHealthAsync(CancellationToken cancellationToken = default);
     }
 }
