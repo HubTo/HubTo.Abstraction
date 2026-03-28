85839433e9900d747e4323e61117c6d30884758a
Jurnest <kyrdrms@gmail.com>
2026-03-28 16:15:07 +0300

    feat: cancelationToken added to auth validators

--- Auth/IHubToAuthValdiator.cs
+++ Auth/IHubToAuthValdiator.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace HubTo.Abstraction.Auth
 {
     public interface IHubToAuthValdiator
     {
-        Task<HubToAuthResult> ValidateApiKeyAsync(string apiKey);
-        Task<HubToAuthResult> ValidateBasicAsync(string username, string password);
+        Task<HubToAuthResult> ValidateApiKeyAsync(string apiKey, CancellationToken cancellationToken = default);
+        Task<HubToAuthResult> ValidateBasicAsync(string username, string password, CancellationToken cancellationToken = default);
     }
 }
